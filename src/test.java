10f665cd2d26efceb25badbb4620c14201f8d1f8
danosaur98 <daniel.zhou2012@gmail.com>
2019-11-09 05:54:07 -0500

    Update test.java

--- src/test.java
+++ src/test.java
@@ -6,9 +6,8 @@ import static org.junit.jupiter.api.Assertions.*;
 public class test {
     @Test
     public void testSolution() {
-        Solution tester = new Solution(); // MyClass is tested
-
-        // assert statements
+        Solution tester = new Solution(); 
+        
         assertTrue(tester.twoSum(new int[]{2, 7, 11, 15}, 9), "2 + 7 = 9");
     }
 }
